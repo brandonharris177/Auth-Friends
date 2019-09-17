e6fa69cb6c38516fabfb367537ece6b133d72899
Brandon-Harris <brandonharris177@gmail.com>
2019-09-17 16:25:18 -0600

    private routes implemented

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import Login from './components/login'
 import mainPage from './components/mainPage'
-// import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Route path="/login" component = {Login} />
       {/* Its Working */}
       {/* <Login /> */}
-      <Route path = "/mainPage" component = {mainPage} />
+      <PrivateRoute path = "/mainPage" component = {mainPage} />
     </div>
   );
 }
